9139e2c8f40c4e05fad8843525f81d855ab56132
J-hoplin1 <jhoplin7259@gmail.com>
2023-12-28 23:21:24 +0900

    Chore: Test

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as bcrypt from 'bcryptjs';
 import { SystemLoggerService } from './system-logger/system-logger.service';
 
 async function bootstrap() {
+  console.log('Test');
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(
